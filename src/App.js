d2121b1966c9a4dda65cd49b4c168a28ad343eab
arshadiqbal06123 <arshadiqbal06123@gail.com>
2023-12-07 23:32:34 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import './App.css';
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
 import Sidebar from './Components/Sidebar/Sidebar';
-import { BrowserRouter as Router } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Router>
         <div className='home-container'>
           <Sidebar />
           <Home />
-        </div>
-      </Router>
+        </div>  
     </div>
   );
 }
